cbb92b3b922c9bee0324d61e46d1c5b87da5cf20
booldook <booldook@gmail.com>
2021-02-22 16:26:28 +0900

    booldook

--- js/index.js
+++ js/index.js
@@ -1,25 +1,12 @@
 /********* 전역선언 **********/
-varscTop, topHeight, mainHeight, winWidth, navi = {};
+var scTop, topHeight, mainHeight, winWidth, navi = {};
 
 /********* 사용자함수 **********/
 function naviShowHide() {
-	if(winWidth >= 1199) { //pc
-		if(scTop >= topHeight + mainHeight) {
-			$(".navi-wrapper").css({"display": "block"});
-			$(".navi-wrapper").css({"position": "fixed"});
-		}
-		else{
-			$(".navi-wrapper").css({"display": "none"});
-			$(".navi-wrapper").css({"position": "relative"});
-		}
-	}
-	else{
-		$(".navi-wrapper").css({"display": "none"});
-	}
+
 }
 
 /********* 이벤트선언 **********/
-mainBanner();	// 배너세팅
 
 $(window).scroll(onScroll); // scroll spy
 $(window).resize(onResize).trigger("resize"); // el 높이, 폭, 위치
@@ -32,9 +19,21 @@ function onResize (e) {
 }
 
 function onScroll (e) {
-	scTop = $(this).scrollTop();
-	naviShowHide();
+	var scTop = $(this).scrollTop();
+	var skillsTop = $(".skills-wrapper").offset().top;
+	console.log(skillsTop, scTop);
+	if(scTop >= skillsTop) {
+		$(".navi-wrapper").addClass('active');
+	}
+	else {
+		$(".navi-wrapper").removeClass('active');
+	}
 }
 
 
-new WOW.init();
+$('.header-wrapper .sub-wrap').click(function(){
+	$('.header-wrapper').toggleClass('active');
+})
+
+
+new WOW().init();
