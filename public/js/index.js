6e6fc957476beeaac213b0dd1b0d0aacd7687c0d
ImHwaryeon <ghkfus1603@naver.com>
2021-03-21 20:37:24 +0900

    ..

--- public/js/index.js
+++ public/js/index.js
@@ -15,10 +15,6 @@ var swiper = new Swiper('.swiper-container', {
 		delay: 3000,
 		disableOnInteraction: false,
 	},
-	pagination: {
-		el: '.swiper-pagination',
-		clickable: true,
-	},
 	navigation: {
 		nextEl: '.swiper-button-next',
 		prevEl: '.swiper-button-prev',
